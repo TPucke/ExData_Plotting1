d68e6bf20750a99cee402538a41ad865cdf448be
tpucke <puckett_thomas@hotmail.com>
2018-03-11 17:00:21 -0400

    plot3 is working

--- plot3.R
+++ plot3.R
@@ -60,6 +60,35 @@ outputfilename <- paste0(strsplit(outputfilename, "\\.")[[1]][1], ".png")
 
 png(filename = outputfilename, width = 480, height = 480)
 
-# Do something here
+plot(x=resultingdata$DateTime, 
+     y=resultingdata$Sub_metering_1,
+     type = "n",
+     xlab = "",
+     ylab = "Energy sub metering"
+)
+
+points(x = resultingdata$DateTime,
+       y = resultingdata$Sub_metering_1,
+       type = "l",
+       col = "black"
+       )
+
+points(x = resultingdata$DateTime,
+       y = resultingdata$Sub_metering_2,
+       type = "l",
+       col = "red"
+)
+
+points(x = resultingdata$DateTime,
+       y = resultingdata$Sub_metering_3,
+       type = "l",
+       col = "blue"
+)
+
+legend("topright",
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1, 1, 1),
+       col = c("black", "red", "blue")
+       )
 
 dev.off()
